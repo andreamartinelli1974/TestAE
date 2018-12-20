997c8854a96e5a00beb894279180cb82260f983a
Andrea Martinelli <andrea.martinelli@bancaimi.com>
2018-12-20 08:43:35 +0100

    working on spot checking

--- Main_AA_TestAutoEncoder_DR.m
+++ Main_AA_TestAutoEncoder_DR.m
@@ -9,7 +9,7 @@ start_date = '01/01/2001';
 end_date = '11/30/2018';
 
 NeuronsNo = 5; % # neurons used 
-useExternalFactors = false;
+useExternalFactors = true;
 useSquaredMyFactorsInTargets = true;
 
 userId = getenv('USERNAME');
@@ -66,7 +66,7 @@ IU.ticker{9,1} = ['NDX Index'];
 
 N_myFactors = size(IU.ticker,1); % when using 'external factor' not included in the AA
 
-% % external factors (to add more explanatory variables)
+% external factors (to add more explanatory variables)
 IU.ticker{10,1} = ['EURUSD Curncy'];           
 IU.ticker{11,1} = ['EURGBP Curncy'];           
 IU.ticker{12,1} = ['EURCHF Curncy']; 
@@ -141,12 +141,13 @@ Invariants.names = IU.ticker(:,1);
 
 % returns and squared returns for the 'core' dataset (my factors only)
 X = [Invariants.data(:,1:N_myFactors)';(Invariants.data(:,1:N_myFactors)').^2]; 
+trainingset = Invariants.data(:,1:N_myFactors)';
 
 % optionally add external explanatory data
 if useExternalFactors
     X = [X;Invariants.data(:,N_myFactors+1:end)'];
+    trainingset = [trainingset;Invariants.data(:,N_myFactors+1:end)'];
 end
- 
 
 targets = (X(1:N_myFactors,:));
 % optionally add the squared residulas to t6he targets (always used in the
@@ -155,138 +156,38 @@ if useSquaredMyFactorsInTargets
     targets = [targets;(Invariants.data(:,1:N_myFactors)').^2];
 end
 
-J = size(X,2);
+%setup of cell array
+XX = matrixTs2CellTs(X);
+targets = matrixTs2CellTs(targets);
 
 disp('creates the basic net')
+% creation of the AutoEncoder object
+AEparams.HiddenSize = 5;
+AEparams.N_myFactors = N_myFactors; % numel(AssetLegend); % number of real factors to be modelled (must be the first n of the data set)
+AEparams.EncoderTransferFunction = 'logsig'; %  'radbas'; %
+AEparams.DecoderTransferFunction = 'purelin';
+AEparams.MaxEpoch = 2500;
+AEparams.ScaleData = false; % true;
+AEparams.divideFcn = 'dividerand'; % Divide data randomly
+AEparams.divideMode = 'time'; % 'sample';
+AEparams.divideParam.trainRatio = 70/100;
+AEparams.divideParam.valRatio = 15/100;
+AEparams.divideParam.testRatio = 15/100;
+AEparams.Delays = [0 1 2 3];
+AEparams.LossFcn = 'mse'; % 'sse'; % 'msesparse'; % Loss function used to train the net
+AEparams.trainFcn = 'trainlm'; % 'trainrp'; % use with mse / sse
+% v.trainFcn = 'trainscg'; % use with msesparse
+AEparams.SquareRet =  true(1); % false(1); %  use also the suared returns in input to catch vola autoreg
 
-hiddenSize_1 = NeuronsNo;
-autoenc1 = trainAutoencoder(X,hiddenSize_1,...
-    'EncoderTransferFunction','logsig', ...
-    'DecoderTransferFunction','purelin', ...
-    'L2WeightRegularization',0.00000,...
-    'SparsityRegularization',1, ...
-    'SparsityProportion',0.40, ...
-    'ScaleData',false);
+AutoEncoder = AutoEncoder_DR(trainingset, AEparams);
 
-% Extract the features in the hidden layer.
-features1 = encode(autoenc1,X); % [13x178] input encoded into [10x178] signals matrix
-
-% 2nd encoding layer layer
-if ~useSquaredMyFactorsInTargets
-    hiddenSize_2 = N_myFactors; 
-else
-    hiddenSize_2 = N_myFactors*2; 
-end
-
-autoenc2 = trainAutoencoder(features1,hiddenSize_2,...
-    'EncoderTransferFunction','logsig', ...
-    'DecoderTransferFunction','purelin', ...
-    'L2WeightRegularization',0.00000,...
-    'SparsityRegularization',1, ...
-    'SparsityProportion',0.40, ...
-    'ScaleData',false);
-
-
-features2 = encode(autoenc2,features1);
-softnet = trainSoftmaxLayer(features2,X,'LossFunction','crossentropy');
-
-% deeper network used for conversion into a NN struct
-% then the NN struct will be fine tuned to reflect the specific needs
-deeperNet = stack(autoenc1,autoenc2,softnet);
-
-% ** deeperNet = train(deeperNet,X,X); % no need to train this obj
-% ** X_hat = decode(autoenc2,features1); % to get back to the original dataset
-
-%% *** CONVERT THE AUTOENCODER INTO A NEURAL NETWORK OBJECT ***
-%      and fine tune its parameters
-%  (then use the function EncDec3LayersWdelays_f to use it for encoding / decoding)
-
-% When working with timeseries in Matlab Neural Network it is better to use 
-% timeseries in cell array form, where each cell corresponds to a point in
-% time. Each cell can contain more than one observed feature wrt the time 
-% it refers to. See comments to the function matrixTs2CellTs
-XX = matrixTs2CellTs(X);
-targets = matrixTs2CellTs(targets); 
-
-% Model training / testing / validation subsamples
-net = network(deeperNet); % transforming into a Matlab neural network object type
-
-net.divideFcn = 'dividerand'; %  % Divide data randomly
-% training, validation and test set proportions
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
-
-% an alternative to 'dividerand'
-% net.divideFcn = 'divideind'; % 'dividerand'; %  % Divide data randomly
-% net.divideMode = 'time'; % 'sample';  % Divide up every sample or in terms of times
-% % training, validation and test set proportions
-% net.divideParam.trainInd = [1:2000];
-% net.divideParam.valInd = [2001:2300];
-% net.divideParam.testInd = [2301:2600];
-
-net.divideMode = 'time'; % 'sample';  % Divide up every sample or in terms of times
-
-% ** net.performParam.normalization = 'standard';
-
-% Plot Functions (see help nnplot) to be used
-net.plotFcns = {'plotperform','plottrainstate','ploterrhist', ...
-    'plotregression', 'plotfit'};
-
-% activation  functions definitions
-net.layers{1}.transferFcn = 'tansig';
-net.layers{2}.transferFcn = 'purelin';
-net.layers{3}.transferFcn = 'purelin';
-
-% time delay in 3rd layer (from 2nd one output)
-net.layerWeights{3,2}.delays = [0:3];
-
-% renaming layers for clarity
-net.layers{1}.name = 'Encoder';
-net.layers{2}.name = 'Decoder';
-net.layers{3}.name = 'Time Delays Layer';
-
-%view(net);
-
-
-%% ********************** IMPORTANTS NETWORK SETTINGS *********************
-% Here the network Loss function and the transfer functions for both layers 
-% are chosen % Also the training function is chosen below: Scaled conjugate 
-% gradient backpropagation is faster bat can be less accurate than the full
-% *Levenberg-Marquardt *backpropagation.
-
-% ***** USING msesparse es Loss Function *****
-% (no delayed nodes allowed with msesparse)
-% net.performFcn = 'msesparse';
-% net.performParam.sparsityRegularization=0;%0;
-% net.performParam.sparsity = 0.0; % 0.050;
-% net.performParam.L2WeightRegularization=0.000000;
-
-% ***** USING mse Loss Function *****
-net.performFcn =  'mse'; %'sse'; %
-net.performParam.regularization=1e-7; % with mse
-
-% net1.trainFcn = 'trainscg'; % use with msesparse
-net.trainFcn = 'trainlm'; % 'trainlm'; % use with mse
-
-%% ********* INITIAL TRAINING (for test) and PLOTTING RESULTS *************
-net1 = configure(net,XX,targets);
-net1.trainParam.epochs = 2000;
-net1.trainParam.max_fail = 6;
-net1.trainParam.showWindow = true(1);
-net1.performParam.regularization = 10e-7;
-
-[Xs,Xi,Ai] = preparets(net1,XX);
-
-tic
-[net1,tr] = train(net1,XX,targets,[],Ai);
-toc
+%view(AutoEncoder.DeeperNet);
 
 % Test the Network
 disp('test the net')
-X_hat_2 = net1(XX);
+X_hat = AutoEncoder.DeeperNet(XX);
 
-X_hat_2 = cell2mat(X_hat_2);
+X_hat = cell2mat(X_hat);
 figure;
 subplot(3,3,1);
 for k=1:N_myFactors
@@ -294,25 +195,14 @@ for k=1:N_myFactors
     plot(X(k,:),'b')
     hold on
     grid on
-    plot(X_hat_2(k,:),'g')
+    plot(X_hat(k,:),'g')
 end
 
-
-% * genFunction(net1,'testFnew'); % USED TO HELP WRITING EncDec3LayersWdelays_f.M
-% * [Y,Xf,Af] = testFnew(XX,[],Ai,original_x);
-
-% prepare data with delays
-[Xs,Xi,Ai] = preparets(net1,XX);
-
-% use EncDec3LayersWdelays_f to separately:
-% 1) extract encoded features
-features = EncDec3LayersWdelays_f(net1,XX,[],Ai,'encode');
-% 2) getting back to trhe original space while also accounting for
-% cross-time and cross sectional autocorrelations and correlations
-x_pred = EncDec3LayersWdelays_f(net1,features,[],Ai,'decode');
-x_pred = cell2mat(x_pred);
+features =  AutoEncoder.EncDecFunction(trainingset,'encode')';
+x_pred =  AutoEncoder.EncDecFunction(features','decode')';
 
 % TEST: compare input data with estimated ones
+
 figure;
 subplot(3,3,1);
 for k=1:N_myFactors
@@ -320,7 +210,7 @@ for k=1:N_myFactors
     plot(X(k,:),'b')
     hold on
     grid on
-    plot(x_pred(k,:),'g')
+    plot(x_pred(:,k),'g')
 end
 
 
@@ -329,68 +219,35 @@ end
 % this is basically a repetition of the step above, but with a fine tuning
 % of the network parameters (some of them) through parametersSpotCheck.m
 disp('spot-check')
-tic
-[optimalParameters,optimalPerformance] = parametersSpotCheck(net,XX,targets);
-toc
+AutoEncoder.parametersSpotCheck(trainingset);
+
 disp('set the final net')
-net1 = configure(net,XX,targets); % to properly set inputs/outputs dimesions (based on X and targets)
+AutoEncoder.SetNet(trainingset);
 
-net1.trainParam.epochs = 2000;
-net1.trainParam.max_fail = 8;
-%net1.trainParam.showWindow = false(1);
-
-if strcmp(net.performFcn,'mse')
-    net1.performParam.regularization = optimalParameters(1);
-elseif strcmp(net.performFcn,'msesparse')
-    net1.performParam.sparsityRegularization = optimalParameters(1);
-    net1.performParam.sparsity = optimalParameters(2);
-    net1.performParam.L2WeightRegularization = optimalParameters(3);
-end
- 
-[Xs,Xi,Ai] = preparets(net1,XX);
-tic
-[net1,tr] = train(net1,XX,targets,[],Ai);
-toc
-% nntraintool('close');
-
-% Test the Network
-X_hat_2 = net1(XX);
-% * Y_hat = sim(net1,X);
-e = gsubtract(targets,X_hat_2);
-perf = perform(net1,X_hat_2,targets) % assess performance
+%test the Network
+X_hat = AutoEncoder.DeeperNet(XX);
+e2 = gsubtract(targets,X_hat);
+perf2 = perform(AutoEncoder.DeeperNet,X_hat,targets) % assess performance
 
 % ************ Training, Validation and Test sets Performance *************
-trainTargets = cell2mat(targets) .* cell2mat(tr.trainMask);
-valTargets = cell2mat(targets) .* cell2mat(tr.valMask);
-testTargets = cell2mat(targets) .* cell2mat(tr.testMask);
-trainPerformance = perform(net1,trainTargets,cell2mat(X_hat_2));
-trainPerformanceInSample = perform(net1,trainTargets,cell2mat(X_hat_2))
-trainPerformanceTest = perform(net1,testTargets,cell2mat(X_hat_2))
 
-valPerformance = perform(net1,valTargets,cell2mat(X_hat_2))
-testPerformance = perform(net1,testTargets,cell2mat(X_hat_2))
+figure, plotperform(AutoEncoder.OUT4Debug.SetNet.tr)
 
-% View the Network
-view(net1)
-
-% Plots
-figure, plotperform(tr)
-
-X_hat_2 = cell2mat(X_hat_2);
+X_hat = cell2mat(X_hat);
 figure;
 subplot(3,3,1);
-for k=1:N_myFactors
-    subplot(3,3,k);
-    plot(X(k,:),'b')
+for kk=1:N_myFactors
+    subplot(3,3,kk);
+    plot(X(kk,10:end),'b')
     hold on
     grid on
-    plot(X_hat_2(k,:),'g')
+    plot(X_hat(kk,10:end),'g')
 end
 
-figure, plotperform(tr)
-figure, plottrainstate(tr)
-figure, ploterrhist(e)
-figure, plotregression(targets,X_hat_2)
+figure, plotperform(AutoEncoder.OUT4Debug.SetNet.tr)
+figure, plottrainstate(AutoEncoder.OUT4Debug.SetNet.tr)
+figure, ploterrhist(e2)
+figure, plotregression(targets,X_hat)
 %% ********* Use the network and its settings to encode / decode **********
 % Once the NN has been trained it can be used through
 % EncDec3LayersWdelays_f.m, either for encoding or decoding purposes
@@ -403,14 +260,11 @@ figure, plotregression(targets,X_hat_2)
 % ncDec3LayersWdelays_f with 'decode'
 
 % ** genFunction(net1,'xxxxxF'); % this can be used to understand how to write a function like 'EncDec3LayersWdelays_f' to use the network
- 
-disp('test the net')
-[Xs,Xi,Ai] = preparets(net1,XX);
-features = EncDec3LayersWdelays_f(net1,XX,[],Ai,'encode');
-x_pred = EncDec3LayersWdelays_f(net1,features,[],Ai,'decode');
-x_pred = cell2mat(x_pred);
 
-highestDelay = net1.layerWeights{3,2}.delays(end); % highest delay used
+disp('test the net')
+features =  AutoEncoder.EncDecFunction(trainingset,'encode')';
+x_pred =  AutoEncoder.EncDecFunction(features','decode')';
+
 
 %% 
 % Obviously x_pred is shorter than the original X by 'highestDelay' elements, 
@@ -423,10 +277,10 @@ figure;
 subplot(3,3,1);
 for k=1:N_myFactors
     subplot(3,3,k);
-    plot(X(k,1:end),'b')
+    plot(X(k,10:end),'b')
     hold on
     grid on
-    plot(x_pred(k,:),'g')
+    plot(x_pred(10:end,k),'g')
 end
 
 % cumulated returns (using exp() since log returns have been modeled)
@@ -434,10 +288,10 @@ figure;
 subplot(3,3,1);
 for k=1:N_myFactors
     subplot(3,3,k);
-    plot(exp(cumsum(X(k,1:end))),'b')
+    plot(exp(cumsum(X(k,10:end))),'b')
     hold on
     grid on
-    plot(exp(cumsum(x_pred(k,:))),'g')
+    plot(exp(cumsum(x_pred(10:end,k))),'g')
 end
 
 %% .... a few more tests on 'features' autocorrelations, compared to initial one
@@ -487,7 +341,7 @@ bicModel<bicNoModel
 
 %%
 % .... same thing wrt features
-F = cell2mat(features);
+F = features';
 nf = size(F,1);
 clear aicNoModel_F bicNoModel_F aicModel_F bicModel_F;
 
@@ -657,6 +511,4 @@ end
 optimalPerformance = performanceLog(mni);
 optimalParameters = testedParameters(mni,:);
 end
-
-
 
